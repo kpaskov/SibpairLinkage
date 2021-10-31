d10d5dcd6ccbbb0bb911125543fc511a2ef931c6
Kelley <kpaskov@stanford.edu>
2021-10-31 11:38:04 -0400

    combinations

--- analysis/permutation_test_phen_comb.py
+++ analysis/permutation_test_phen_comb.py
@@ -79,7 +79,7 @@ print('ready')
 
 aut_response = ['0.0']*2 + ['1.0']*6 + ['0.0'] + ['1.0']*9 + ['0.0']*22
 
-for phen_index2 in range(40):
+for phen_index2 in range(phen_index+1, 40):
 	print('SCQ', phen_index, phen_index2)
 	phen_indices = [phen_index, phen_index2]
 
